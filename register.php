6475b3778937fde684cb261bf3efac994389d357
harolddalusong <hdalusong23-0303@cca.edu.ph>
2025-02-26 18:14:11 +0800

    ghghghg
    ghhg

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
@@ -10,7 +11,7 @@
 </body>
 </html>
 <form action="action.php" method="POST">
-    <table>
+    <table border="1"> 
         <tr>
             <td> Name: </td>
             <td> <input type="text" name="name"> </td>
